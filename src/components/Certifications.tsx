52c58e64788478eb29fe75cdea026c9c5d72da13
shihab-bhuiya <shihab.bhuiya07@gmail.com>
2026-09-21 21:22:59 +0600

    refactor: adjust width of certificate icons for improved layout consistency

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -64,7 +64,7 @@ export default function Certificates() {
                 relative
                 flex
                 h-[140px]
-                w-[10px]
+                w-[120px]
                 mx-auto
                 max-w-[1312px]
                 flex-col
